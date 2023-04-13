6ed221e1553e8163807e678713e1b5e33de25d76
mariobac1 <mario.moralesbac1@gmail.com>
2023-04-13 11:58:10 -0400

    Se agregan rutas para consumir png de redes sociales

--- infrastructure/handler/routes.go
+++ infrastructure/handler/routes.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -14,6 +16,8 @@ import (
 	"github.com/mariobac1/backend_webpages/infrastructure/handler/user"
 )
 
+const pathIcon = "./public/icons/social_network/"
+
 func InitRoutes(e *echo.Echo, dbPool *pgxpool.Pool) {
 	health(e)
 	// A
@@ -23,20 +27,35 @@ func InitRoutes(e *echo.Echo, dbPool *pgxpool.Pool) {
 	//...
 	// E
 	// F
+	facebookIcon(e)
+	facebookGrayIcon(e)
 	// L
 	login.NewRouter(e, dbPool)
 	// H
 	imagehome.NewRouter(e, dbPool)
 	//I
+	instagramIcon(e)
+	instagramGrayIcon(e)
 	// image(e)
+	//M
+	msnFBIcon(e)
+	msnFBGrayIcon(e)
 	// P
 	product.NewRouter(e, dbPool)
 	// R
 	// S
 	// sendImage(e)
 	// T
+	twitterIcon(e)
+	twitterGrayIcon(e)
 	// U
 	user.NewRouter(e, dbPool)
+	// V
+	// W
+	whatsappIcon(e)
+	whatsappGrayIcon(e)
+	// Y
+	// Z
 }
 
 func health(e *echo.Echo) {
@@ -118,19 +137,172 @@ func health(e *echo.Echo) {
 // 	}
 // }
 
-// func sendImage(e *echo.Echo) {
-// 	e.GET("/api/v1/image", func(c echo.Context) error {
-// 		var imagePath string
-// 		files, err := ioutil.ReadDir("imagenes")
-// 		if err != nil {
-// 			return err
-// 		}
-// 		for _, f := range files {
-// 			if strings.HasPrefix(f.Name(), "logo") {
-// 				imagePath = "imagenes/" + f.Name()
-// 				break
-// 			}
-// 		}
-// 		return c.File(imagePath)
-// 	})
-// }
+func facebookIcon(e *echo.Echo) {
+	e.GET("/api/v1/facebookIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "facebook") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func facebookGrayIcon(e *echo.Echo) {
+	e.GET("/api/v1/facebookGrayIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "facebook_gray") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func twitterIcon(e *echo.Echo) {
+	e.GET("/api/v1/TwitterIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "twitter") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func twitterGrayIcon(e *echo.Echo) {
+	e.GET("/api/v1/TwitterGrayIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "twitter_gray") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func instagramIcon(e *echo.Echo) {
+	e.GET("/api/v1/instagramIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "instagram") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func instagramGrayIcon(e *echo.Echo) {
+	e.GET("/api/v1/instagramGrayIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "instagram_gray") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func whatsappIcon(e *echo.Echo) {
+	e.GET("/api/v1/whatsappIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "whatsapp") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func whatsappGrayIcon(e *echo.Echo) {
+	e.GET("/api/v1/whatsappGrayIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "whatsapp_gray") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func msnFBIcon(e *echo.Echo) {
+	e.GET("/api/v1/msnfbIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "fb_messenger") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
+
+func msnFBGrayIcon(e *echo.Echo) {
+	e.GET("/api/v1/msnfbGrayIcon", func(c echo.Context) error {
+		var imagePath string
+		files, err := ioutil.ReadDir(pathIcon)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "fb_messenger") {
+				imagePath = pathIcon + f.Name()
+				break
+			}
+		}
+		return c.File(imagePath)
+	})
+}
